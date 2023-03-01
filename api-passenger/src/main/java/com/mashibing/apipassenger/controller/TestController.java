a2f0c2afcdf7c4395930c6c61c81978d13cda0f4
yinwei9 <975103637@qq.com>
2023-03-01 15:38:28 +0800

    第二次提交

--- api-passenger/src/main/java/com/mashibing/apipassenger/controller/TestController.java
+++ api-passenger/src/main/java/com/mashibing/apipassenger/controller/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
     @GetMapping("/test")
-    public String test() {
-        return "aaa";
+    public String test2() {
+        return "asfdjasdjflkajslfk";
     }
 }
